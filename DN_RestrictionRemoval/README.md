c6158358dc515046bfeff509d815847b4d57dd46
Albin Hallden <albin.hallden@cybercom.com>
2016-08-03 08:59:11 +0200

    Add DN restriction removal script

---
+++ DN_RestrictionRemoval/README.md
@@ -0,0 +1,3 @@
+# DN (Dagens Nyheter) - Article block removal
+
+This script will remove the client side article read restriction. 
---
+++ DN_RestrictionRemoval/RestrictionRemoval.js
@@ -0,0 +1,3 @@
+document.querySelector('.article__body')
+  .classList.remove('article__body--mask'); 
+document.querySelector('.js-paywall.paywall').remove();
